5d673a12eb9d2cc277349ec12e5bcc329ef19ed1
Neha Upadhyay <nehau072@gmail.com>
2026-09-06 16:36:34 +0530

    Fix home inport fot production build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 
 function App() {
   const isLoggedIn = () => {
