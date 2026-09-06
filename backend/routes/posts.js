6814b442867208f02295b27a993b78be5a9e1c39
Neha Upadhyay <nehau072@gmail.com>
2026-09-06 15:57:50 +0530

    Fix post model import case

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-const Post = require("../models/Post");
+const Post = require("../models/post");
 
 const router = express.Router();
 
